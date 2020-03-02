a80dd8c64483fea7f473fea2f532b9ffe5625c25
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-02 12:24:51 +0530

    Update leetsoln.py

--- leetsoln.py
+++ leetsoln.py
@@ -1,15 +1,15 @@
 array = list(map(int,input().split()))
 n = int(input())
-left,right = [],[]
+length = len(array)
+left,result = [],[]
 x = 1
 left.append(x)
-for i in range(1,len(array)):
+for i in range(1,length):
     x *= array[i-1]
     left.append(x)
 x = 1
-right.append(x)
-for i in range(len(array)-1,0,-1):
+result.append(x*array[length-1])
+for i in range(length-1,0,-1):
     x *= array[i]
-    right.insert(0,x)
-
-print(left[n]*right[n])
+    result.insert(0,x*left[i-1])
+print(result[n])
