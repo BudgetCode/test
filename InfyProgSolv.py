8c132e5273b717e74c7bc9893f9707aaa131ec8b
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-03 17:06:00 +0530

    Appropriate Variable Name changes

--- InfyProgSolv.py
+++ InfyProgSolv.py
@@ -10,11 +10,11 @@ for i in range(1,number):
 	if x > number: break
 	x = str(x)
 	if x in strNum:
-		if iter >= 2:
-			iter = 0
-			count+=1
+		if count >= 2:
+			count = 0
+			iter+=1
 			nRes.append(dict())
-		nRes[count][x] = (i,i+1)
-	else: iter+=1
+		nRes[iter][x] = (i,i+1)
+	else: count+=1
 
 print(max(nRes,key=lambda x:len(x)))
