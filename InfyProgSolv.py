d8db9fed910e7d71e9d805d25394bce51810979e
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-03 15:49:31 +0530

    Optimized by Reducing Loop iterations

--- InfyProgSolv.py
+++ InfyProgSolv.py
@@ -2,12 +2,14 @@
 take an input say (12630). Find out all the numbers in this input which were a multiplication of consecutive numbers. It included 2(1*2), 6(2*3), 12(3*4), 30(5*6).
 """
 
-number , x = int(input()) , 0
+number = int(input()) 
+x = count = 0
 strNum = str(number)
 nRes = dict()
 for i in range(1,number):
 	x = i*(i+1)
-	if x > number: break
+	if x > number or count >= 2: break
 	x = str(x)
 	if x in strNum: nRes[x] = (i,i+1)
+	else: count +=1
 print(nRes)
