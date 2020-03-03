5e5abb85bf3471bc05685ac5a51a58d5bc35fbaa
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-03 16:40:13 +0530

    Fixed checking only from start
    Now the script checks for the longest chain anywhere in generated numbers instead of just from the start.

--- InfyProgSolv.py
+++ InfyProgSolv.py
@@ -1,15 +1,20 @@
 """
 take an input say (12630). Find out all the numbers in this input which were a multiplication of consecutive numbers. It included 2(1*2), 6(2*3), 12(3*4), 30(5*6).
 """
-
 strNum = input()
-number = int(strNum)
-x = count = 0
-nRes = dict()
+number = int(strNum) 
+x = count = iter = 0
+nRes = [dict()]
 for i in range(1,number):
 	x = i*(i+1)
-	if x > number or count >= 2: break
+	if x > number: break
 	x = str(x)
-	if x in strNum: nRes[x] = (i,i+1)
-	else: count +=1
-print(nRes)
+	if x in strNum:
+		if iter >= 2:
+			iter = 0
+			count+=1
+			nRes.append(dict())
+		nRes[count][x] = (i,i+1)
+	else: iter+=1
+
+print(max(nRes,key=lambda x:len(x)))
