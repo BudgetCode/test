4cb509cc20f8322ea022c5e4dd9b367e7fec857c
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-03 18:31:45 +0530

    Update InfyProgSolv.py

--- InfyProgSolv.py
+++ InfyProgSolv.py
@@ -1,13 +1,16 @@
+
 """
-take an input say (12630). Find out all the longest sequence of numbers in this input which were a multiplication of consecutive numbers. It included 2(1*2), 6(2*3), 12(3*4), 30(5*6).
+take an input say (12630). Find out the longest sequence numbers in this input which were a multiplication of consecutive numbers. It included 2(1*2), 6(2*3), 12(3*4), 30(5*6).
 """
+
+from math import sqrt ,ceil
+
 strNum = input()
-number = int(strNum) 
+number = ceil(sqrt(int(strNum)))
 x = count = iter = 0
 nRes = [dict()]
 for i in range(1,number):
 	x = i*(i+1)
-	if x > number: break
 	x = str(x)
 	if x in strNum:
 		if count >= 2:
