418191b4c4c8dd60a11948a4ee2ee7bd1b2c73e2
Anurag Singh <30367300+BudgetCode@users.noreply.github.com>
2020-03-08 01:16:19 +0530

    Add files via upload

---
+++ subsetsc.py
@@ -0,0 +1,18 @@
+n = int(input())
+array = list(map(int,input().split()))
+count = 2**(n-1)
+negArr = len([i for i in array if i < 0])
+
+#[-1] -> [[] [-1]] 1
+#[-1,-2] -> [[][-1][-2][-1,-2]] 2
+#[-1,-2,-3] -> [[][-1][-2][-3],[-1,-2],[-1,-3],[-3,-2],[-1,-3,-2]] 4
+#[-1,2,-3] -> [[][-1][2][-3],[-1,2],[-1,-3],[-3,2],[-1,-3,2]] 4 
+#[-1,2,-3] -> [[][1][2][-3],[1,2],[1,-3],[-3,2],[1,-3,2]] 4 
+
+
+from itertools import chain, combinations
+x = len(array)
+x = [[array[j] for j in range(x) if (i & (1 << j))] for i in range(1<<x)][1:]
+n =  [m for m in x if len(list(filter(lambda y:(y<0),m)))%2 != 0] # list of all the valid subsets with odd no. of negatives
+sn = len(n)
+print(sn)
